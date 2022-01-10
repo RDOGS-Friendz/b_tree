cddb0c9dd06e39cb611ec87fb54b5682dc59dd11
easonyu0203 <62385417+easonyu0203@users.noreply.github.com>
2022-01-10 15:13:12 +0800

    working on visualize tree

---
+++ lib/bTree.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+
+enum BuildTreeTypeEnum {
+  bottomUp,
+  oneByOne,
+}
+
+class Node {
+  int order;
+  List<int> values = [];
+  List<Node?> keys = [];
+  Node? parent;
+  bool checkLeaf = false;
+  Box? box;
+
+  Node(this.order);
+
+  String valueStr() {
+    return values.map<String>((e) => "$e").toList().join(" ");
+  }
+
+  void insertAtLeaf(int value) {
+    if (values.isNotEmpty) {
+      for (var i = 0; i < values.length; i++) {
+        if (value == values[i]) {
+          // ignore
+          break;
+        } else if (value < values[i]) {
+          values.insert(i, value);
+          break;
+        } else if (i + 1 == values.length) {
+          values.add(value);
+          break;
+        }
+      }
+    } else {
+      values.add(value);
+    }
+  }
+}
+
+class BTree {
+  Node? root;
+
+  BTree(int order) {
+    root = Node(order);
+    root!.checkLeaf = true;
+  }
+
+  void inSert(int v) {
+    var oldNode = search(v);
+    oldNode.insertAtLeaf(v);
+
+    if (oldNode.values.length == oldNode.order) {
+      var node1 = Node(oldNode.order);
+      node1.checkLeaf = true;
+      node1.parent = oldNode.parent;
+      int mid = (oldNode.order / 2.0).ceil() - 1;
+      node1.values = oldNode.values.sublist(mid + 1);
+      oldNode.values = oldNode.values.sublist(0, mid + 1);
+      insertInParent(oldNode, node1.values[0], node1);
+    }
+  }
+
+  Node search(int v) {
+    Node? currentNode = root!;
+    while (currentNode!.checkLeaf == false) {
+      for (var i = 0; i < currentNode!.values.length; i++) {
+        if (v == currentNode.values[i]) {
+          currentNode = currentNode.keys[i + 1];
+          break;
+        } else if (v < currentNode.values[i]) {
+          currentNode = currentNode.keys[i];
+          break;
+        } else if (i + 1 == currentNode.values.length) {
+          currentNode = currentNode.keys[i + 1];
+          break;
+        }
+      }
+    }
+
+    return currentNode;
+  }
+
+  void insertInParent(Node oldNode, int v, Node node1) {
+    if (root == oldNode) {
+      root = Node(oldNode.order);
+      root?.values = [v];
+      root?.keys = [oldNode, node1];
+      oldNode.parent = root;
+      node1.parent = root;
+      return;
+    }
+
+    Node parentNode = oldNode.parent!;
+    for (var i = 0; i < parentNode.keys.length; i++) {
+      if (parentNode.keys[i] == oldNode) {
+        parentNode.values.insert(i, v);
+        parentNode.keys.insert(i + 1, node1);
+
+        if (parentNode.keys.length > parentNode.order) {
+          Node parentDash = Node(parentNode.order);
+          parentDash.parent = parentNode.parent;
+          int mid = (parentNode.order / 2.0).ceil() - 1;
+          parentDash.values = parentNode.values.sublist(mid + 1);
+          parentDash.keys = parentNode.keys.sublist(mid + 1);
+          int valueDash = parentNode.values[mid];
+          parentNode.values = parentNode.values.sublist(0, mid);
+          parentNode.keys = parentNode.keys.sublist(0, mid + 1);
+          for (var i = 0; i < parentNode.keys.length; i++) {
+            parentNode.keys[i]?.parent = parentNode;
+          }
+          for (var i = 0; i < parentDash.keys.length; i++) {
+            parentDash.keys[i]?.parent = parentDash;
+          }
+          insertInParent(parentNode, valueDash, parentDash);
+        }
+      }
+    }
+  }
+
+  List<List<Node>> getLevel() {
+    List<List<Node>> levels = [[]];
+    Node node = root!;
+
+    // get level cnt
+    while (node.checkLeaf != true) {
+      levels.add([]);
+      node = node.keys[0]!;
+    }
+
+    node = root!;
+    addLevels(node, 0, levels);
+
+    return levels;
+  }
+
+  void addLevels(Node node, int level, List<List<Node>> levels) {
+    node.box = Box(node.valueStr());
+    levels[level].add(node);
+    if (node.checkLeaf == false) {
+      for (var k in node.keys) {
+        addLevels(k!, level + 1, levels);
+      }
+    }
+  }
+}
+
+class Box {
+  // SETTING
+  final boxLinePaint = Paint()
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 2;
+  final textStyle = TextStyle(
+    color: Colors.black,
+    fontSize: 20,
+  );
+
+  String content;
+  TextPainter? textPainter;
+  Size? size;
+  final boxPadding = 5.0;
+
+  Box(
+    this.content,
+  ) {
+    final textSpan = TextSpan(
+      text: content,
+      style: textStyle,
+    );
+    textPainter = TextPainter(
+      text: textSpan,
+      textDirection: TextDirection.ltr,
+    );
+    textPainter?.layout(
+      minWidth: 0,
+      maxWidth: double.infinity,
+    );
+    size = Size(textPainter!.width + 2 * boxPadding,
+        textPainter!.height + 2 * boxPadding);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,17 @@
 
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
-import 'dart:convert' show utf8;
-import 'dart:async';
 import 'dart:io';
+import './bTree.dart';
 
 void main() {
+  BTree bTree = BTree(3);
+  bTree.inSert(5);
+  bTree.inSert(15);
+  bTree.inSert(25);
+  bTree.inSert(35);
+  bTree.inSert(45);
   runApp(const MyApp());
-}
-
-enum BuildTreeTypeEnum {
-  bottomUp,
-  oneByOne,
 }
 
 String buildTreeTypeString(BuildTreeTypeEnum buildTreeType) {
@@ -38,6 +38,9 @@ class _MyAppState extends State<MyApp> {
 
   // states
   BuildTreeTypeEnum choosedBuildTreeType = BuildTreeTypeEnum.bottomUp;
+  int maxDegree = 3;
+  List<int> values = [];
+  BTree? bTree;
 
   // private method
   void setBuildTreeType(BuildTreeTypeEnum? buildTreeTypeEnum) {
@@ -55,10 +58,29 @@ class _MyAppState extends State<MyApp> {
         path = path.substring(21);
       }
       File file = File(path);
-      String data = await file.readAsString();
+      String dataStr = (await file.readAsString());
+      List<int> data =
+          dataStr.split(" ").map<int>((e) => int.parse(e)).toList();
+
+      // action of after choose file
+      BuildBTree(data, choosedBuildTreeType);
+      setState(() {
+        values = data;
+      });
     } else {
       // User canceled the picker
     }
+  }
+
+  void BuildBTree(List<int> values, BuildTreeTypeEnum buildTreeType) {
+    BTree bTree = BTree(maxDegree);
+    for (int v in values) {
+      bTree.inSert(v);
+    }
+
+    setState(() {
+      this.bTree = bTree;
+    });
   }
 
   @override
@@ -87,6 +109,27 @@ class _MyAppState extends State<MyApp> {
                     color: Colors.orange[200],
                     child: Column(
                       children: [
+                        const Text("Choose Max. Degree:"),
+                        DropdownButton(
+                          value: maxDegree,
+                          items: [3, 4, 5, 6, 7]
+                              .map<DropdownMenuItem<int>>((e) =>
+                                  DropdownMenuItem<int>(
+                                      value: e, child: Text('${e}')))
+                              .toList(),
+                          onChanged: (int? e) {
+                            setState(() {
+                              maxDegree = e!;
+                            });
+                          },
+                          icon: const Icon(Icons.arrow_downward),
+                          elevation: 16,
+                          style: const TextStyle(color: Colors.deepPurple),
+                          underline: Container(
+                            height: 2,
+                            color: Colors.deepPurpleAccent,
+                          ),
+                        ),
                         const Text("Choose Build Tree Method:"),
                         DropdownButton(
                           value: choosedBuildTreeType,
@@ -109,14 +152,26 @@ class _MyAppState extends State<MyApp> {
                           style: ElevatedButton.styleFrom(
                               textStyle: const TextStyle(fontSize: 20)),
                           onPressed: userChooseFile,
-                          child: const Text('Enabled'),
+                          child: const Text('Choose File'),
                         ),
                       ],
                     ),
                   ),
                   Expanded(
                     child: Container(
-                      color: Colors.orange[50],
+                      color: Colors.orange[100],
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40, vertical: 40),
+                      child: Container(
+                        color: Colors.brown[200],
+                        child: (bTree == null)
+                            ? Container()
+                            : CustomPaint(
+                                size: const Size(
+                                    double.infinity, double.infinity),
+                                painter: BTreePainter(bTree!),
+                              ),
+                      ),
                     ),
                   )
                 ],
@@ -126,5 +181,122 @@ class _MyAppState extends State<MyApp> {
         ),
       ),
     );
+  }
+}
+
+class Box {
+  // SETTING
+  final boxLinePaint = Paint()
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 2;
+  final textStyle = TextStyle(
+    color: Colors.black,
+    fontSize: 20,
+  );
+
+  String content;
+  TextPainter? textPainter;
+  Size? size;
+  final boxPadding = 5.0;
+
+  Box(this.content, Size cSize) {
+    final textSpan = TextSpan(
+      text: content,
+      style: textStyle,
+    );
+    textPainter = TextPainter(
+      text: textSpan,
+      textDirection: TextDirection.ltr,
+    );
+    textPainter?.layout(
+      minWidth: 0,
+      maxWidth: double.infinity,
+    );
+    size = Size(textPainter!.width + 2 * boxPadding,
+        textPainter!.height + 2 * boxPadding);
+  }
+}
+
+class BTreePainter extends CustomPainter {
+  // SETTING
+  final boxLinePaint = Paint()
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 2;
+  final textStyle = TextStyle(
+    color: Colors.black,
+    fontSize: 20,
+  );
+  final boxPadding = 5.0;
+  final levelDist = 70.0;
+  final topMargin = 50.0;
+  final sideMargin = 20.0;
+
+  // state
+  BTree bTree;
+
+  BTreePainter(this.bTree);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // draw b tree
+    List<List<Node>> levels = bTree.getLevel();
+    List<List<Box>> levelBoxes = bTree
+        .getLevel()
+        .map<List<Box>>(
+            (e) => e.map<Box>((e) => Box(e.valueStr(), size)).toList())
+        .toList();
+    // draw bottom first
+    // 1. get bottom level total width
+    double buttomWidth = -sideMargin;
+    int levelCnt = levelBoxes.length;
+    for (Box box in levelBoxes[levelCnt - 1]) {
+      buttomWidth += box.size!.width + sideMargin;
+    }
+    // 2. draw bottom level
+    double xOff = size.width / 2 - buttomWidth / 2;
+    for (int i = 0; i < levelBoxes[levelCnt - 1].length; i++) {
+      double preBoxWidth =
+          (i != 0) ? levelBoxes[levelCnt - 1][i - 1].size!.width : 0;
+      Box box = levelBoxes[levelCnt - 1][i];
+      xOff += box.size!.width / 2;
+      drawBoxText(box.content,
+          Offset(xOff, topMargin + levelDist * (levelCnt - 1)), canvas, size);
+      xOff += box.size!.width / 2 + sideMargin;
+    }
+    // 3. draw by buttom up
+    for (int i = levelCnt - 2; i >= 0; i--) {}
+  }
+
+  @override
+  bool shouldRepaint(BTreePainter oldDelegate) => false;
+
+  Size drawBoxText(String t, Offset offset, Canvas canvas, Size cSize) {
+    final textSpan = TextSpan(
+      text: t,
+      style: textStyle,
+    );
+    final textPainter = TextPainter(
+      text: textSpan,
+      textDirection: TextDirection.ltr,
+    );
+    textPainter.layout(
+      minWidth: 0,
+      maxWidth: cSize.width,
+    );
+    double x = offset.dx, y = offset.dy;
+    x -= textPainter.width / 2;
+    y -= textPainter.height / 2;
+
+    textPainter.paint(canvas, Offset(x, y));
+    x -= boxPadding / 2;
+    y -= boxPadding / 2;
+    canvas.drawRect(
+        Offset(x, y) &
+            Size(textPainter.width + boxPadding,
+                textPainter.height + boxPadding),
+        boxLinePaint);
+
+    return Size((textPainter.width + boxPadding) / 2.0,
+        (textPainter.height + boxPadding) / 2.0);
   }
 }
